62c9b418193ab1aa15caede5ca7bb5bcd3857c49
Jorge Vacarisas bofill <jvacaris@cbr4s6.42barcelona.com>
2025-01-20 18:12:12 +0100

    Warning fixes

--- bonus/estimatePrice.py
+++ bonus/estimatePrice.py
@@ -7,23 +7,22 @@ def error_and_exit(line1, line2=None):
 		print(">>", line2, "<<")
 	exit(1)
 
-def estimatePrice(mileage=None, theta0=None, theta1=None):
+def estimatePrice(mileage=None, theta0=None, theta1=None, gen_error=None):
 	if mileage == None:
-		print("Error: Please introduce a mileage as an input.")
-		exit(1)
+		error_and_exit("Please introduce a mileage as an input.")
 	try:
 		mileage = float(mileage)
+		if mileage < 0 and gen_error == True:
+			print("Warning: Mileage goes below 0. This would make no sense IRL but will calculate it anyway.")
 	except ValueError:
-		print("Error: Mileage given must be a number.")
-		exit(1)
+		error_and_exit("Mileage given must be a number.")
 	if theta0 == None or theta1 == None:
 		try:
 			file = open("vars", "r")
 			text = file.read()
 			file.close()
 		except:
-			print("Error: Unable to open a vars file")
-			exit(1)
+			error_and_exit("Unable to open a vars file.")
 		text = [x.split(" = ") for x in text.split("\n")]
 		for var in text:
 			if var[0] == "theta0":
@@ -35,14 +34,11 @@ def estimatePrice(mileage=None, theta0=None, theta1=None):
 			theta0 = float(theta0)
 			theta1 = float(theta1)
 		except ValueError:
-			print("Error: θs given must be numbers.")
-			exit(1)
+			error_and_exit("θs given must be numbers.")
 
 	if theta0 == None or theta1 == None:
-		print("Error: Unable to retrieve the variable's values.")
-		exit(1)
+		error_and_exit("Unable to retrieve the variable's values.")
 
-	# print(float(mileage), theta0, theta1)
 	return(theta0 + mileage * theta1)
 
 
@@ -88,9 +84,9 @@ def generateGraph(estimatedPrice, mileage):
 if __name__=="__main__":
 	estimatedPrice = 0
 	if len(sys.argv) == 2:
-		estimatedPrice = estimatePrice(sys.argv[1])
+		estimatedPrice = estimatePrice(sys.argv[1], gen_error=True)
 	elif len(sys.argv) == 4:
-		estimatedPrice = estimatePrice(sys.argv[1], sys.argv[2], sys.argv[3])
+		estimatedPrice = estimatePrice(sys.argv[1], sys.argv[2], sys.argv[3], gen_error=True)
 	elif len(sys.argv) < 2: 
 		error_and_exit("Not enough arguments provided.")
 	elif len(sys.argv) == 3: 
@@ -99,6 +95,8 @@ if __name__=="__main__":
 		error_and_exit("Too many arguments provided.")
 
 	print("Estimated price:", estimatedPrice, "₳")
+	if estimatedPrice < 0.0:
+		print("Warning: Estimated price goes below 0. This should not make sense IRL but the excercise allows it.")
 	print("Generating graph...")
 	generateGraph(estimatedPrice, int(sys.argv[1]))
 
--- mandatory/estimatePrice.py
+++ mandatory/estimatePrice.py
@@ -1,11 +1,13 @@
 import sys
 
-def estimatePrice(mileage=None, theta0=None, theta1=None):
+def estimatePrice(mileage=None, theta0=None, theta1=None, gen_error=None):
 	if mileage == None:
 		print("Error: Please introduce a mileage as an input.")
 		exit(1)
 	try:
 		mileage = float(mileage)
+		if mileage < 0 and gen_error == True:
+			print("Warning: Mileage goes below 0. This would make no sense IRL but will calculate it anyway.")
 	except ValueError:
 		print("Error: Mileage given must be a number.")
 		exit(1)
@@ -35,18 +37,20 @@ def estimatePrice(mileage=None, theta0=None, theta1=None):
 		print("Error: Unable to retrieve the variable's values.")
 		exit(1)
 
-	# print(float(mileage), theta0, theta1)
 	return(theta0 + mileage * theta1)
 
 
 if __name__=="__main__":
 	if len(sys.argv) == 2:
-		print("Estimated price:", estimatePrice(sys.argv[1]), "₳")
+		print("Estimated price:", estimatePrice(sys.argv[1], gen_error=True), "₳")
 	elif len(sys.argv) == 4:
-		print("Estimated price:", estimatePrice(sys.argv[1], sys.argv[2], sys.argv[3]), "₳")
+		print("Estimated price:", estimatePrice(sys.argv[1], sys.argv[2], sys.argv[3], gen_error=True), "₳")
 	elif len(sys.argv) < 2: 
 		print("Not enough arguments provided.")
+		exit(1)
 	elif len(sys.argv) == 3: 
 		print("Wrong amount of arguments provided.")
+		exit(1)
 	else:
 		print("Too many arguments provided.")
+		exit(1)
